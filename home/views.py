8648cffbbdf3061876105657bdba87f781714bc9
pratik singh <1809193@sbsstc.ac.in>
2021-06-19 23:01:41 +0530

    v1.0

--- home/views.py
+++ home/views.py
@@ -68,15 +68,6 @@ def login(request):
 
 
 def cart(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
-
     return render(request, 'cart.html')
 
 
